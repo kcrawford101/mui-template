d7e07342da031f777f45fac16bc4d6934cb88b45
Kevin Crawford <crawfordkevin101@gmail.com>
2022-01-14 14:38:02 -0500

    added some styling changes to Sign in page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Login from './Components/Login/Login';
+// import Login2 from './Components/Login2/Login2';
 
 function App() {
   return (
     <div>
     <Login />
+    {/* <Login2 /> */}
     </div>
   );
 }
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,24 +1,29 @@
 import React from "react";
 import { Grid, Paper, Avatar, TextField, Button, Typography } from '@material-ui/core';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 
 function Login() {
 
-    const paperStyle={padding: 20, height: 'fit-content', width: 400, margin: '20px auto'}
-    const avatarStyle={backgroundColor: 'green'}
+    const paperStyle={padding: 20, height: 'fit-content', width: 500, margin: '20px auto'}
+    const avatarStyle={backgroundColor: 'green', margin:'8px 0', width: 56, height: 56}
     const btnStyle={margin:'8px 0'}
+    const logo={fontSize:'175px', margin:'15px 0 0'}
+    const logoName={margin:'0 0 50px'}
 
     return(
 
-        <Grid>            
+        <Grid>
+            <Grid align='center'>
+                <AcUnitIcon style={logo} /><Typography style={logoName} variant='h2'>Cold Order Form</Typography>
+            </Grid>            
             <Paper elevation={10} style={paperStyle}>
                 <Grid align='center'>
-                <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>                                
-                <h2>Sign In</h2>
+                <Avatar style={avatarStyle}>A</Avatar>                                
+                <Typography variant='h3'>Sign In</Typography>
                 </Grid>
                 <TextField label='Email' placeholder='Enter Email' fullWidth required/>
                 <TextField label='Password' placeholder='Enter Password' type='password'fullWidth required/>
